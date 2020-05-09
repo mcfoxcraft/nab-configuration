f75aec1c297f37983999cf1950e9409fcbf64564
froobynooby <froobynooby@froobworld.com>
2020-05-09 18:59:14 +0930

    Add ConfigEntries for some common config entries

---
+++ src/main/java/com/froobworld/nabconfiguration/ConfigEntries.java
@@ -0,0 +1,45 @@
+package com.froobworld.nabconfiguration;
+
+public final class ConfigEntries {
+
+
+    public static ConfigEntry<Double> doubleEntry() {
+        return new ConfigEntry<>(o -> ((Number) o).doubleValue());
+    }
+
+    public static ConfigEntry<Float> floatEntry() {
+        return new ConfigEntry<>(o -> ((Number) o).floatValue());
+    }
+
+    public static ConfigEntry<Long> longEntry() {
+        return new ConfigEntry<>(o -> ((Number) o).longValue());
+    }
+
+    public static ConfigEntry<Integer> integerEntry() {
+        return new ConfigEntry<>(o -> ((Number) o).intValue());
+    }
+
+    public static ConfigEntry<Short> shortEntry() {
+        return new ConfigEntry<>(o -> ((Number) o).shortValue());
+    }
+
+    public static ConfigEntry<Byte> byteEntry() {
+        return new ConfigEntry<>(o -> ((Number) o).byteValue());
+    }
+
+    public static <E extends Enum<E>> ConfigEntry<E> enumEntry(Class<E> e) {
+        return enumEntry(e, false);
+    }
+
+    public static <E extends Enum<E>> ConfigEntry<E> enumEntry(Class<E> e, boolean caseSensitive) {
+        return new ConfigEntry<>(o -> {
+            for (E value : e.getEnumConstants()) {
+                if (caseSensitive ? value.name().equals(o.toString()) : value.name().equalsIgnoreCase(o.toString())) {
+                    return value;
+                }
+            }
+            return null;
+        });
+    }
+
+}
