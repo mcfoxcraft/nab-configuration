dd985fe3e2a54e8f0777dcb3cd9281817c8bab04
froobynooby <froobynooby@froobworld.com>
2020-12-20 14:46:21 +0930

    Setting to ignore key cases

--- src/main/java/com/froobworld/nabconfiguration/ConfigEntryMap.java
+++ src/main/java/com/froobworld/nabconfiguration/ConfigEntryMap.java
@@ -10,15 +10,19 @@ public class ConfigEntryMap<K, T> {
     private final Map<String, ConfigEntry<T>> entryMap = new HashMap<>();
     private final Supplier<ConfigEntry<T>> emptyConfigEntrySupplier;
     private final ConfigEntry<T> def;
+    private final boolean ignoreCase;
 
-    public ConfigEntryMap(Function<K, String> keyMappingFunction, Supplier<ConfigEntry<T>> emptyConfigEntrySupplier) {
+    public ConfigEntryMap(Function<K, String> keyMappingFunction, Supplier<ConfigEntry<T>> emptyConfigEntrySupplier, boolean ignoreCase) {
         this.keyMappingFunction = keyMappingFunction;
         this.emptyConfigEntrySupplier = emptyConfigEntrySupplier;
         this.def = emptyConfigEntrySupplier.get();
+        this.ignoreCase = ignoreCase;
     }
 
     public ConfigEntry<T> of(K key) {
-        return entryMap.getOrDefault(keyMappingFunction.apply(key), def);
+        String mappedKey = keyMappingFunction.apply(key);
+        mappedKey = ignoreCase ? mappedKey.toLowerCase() : mappedKey;
+        return entryMap.getOrDefault(mappedKey, def);
     }
 
     void setDefault(Object def) {
@@ -26,6 +30,7 @@ public class ConfigEntryMap<K, T> {
     }
 
     void put(String key, Object value) {
+        key = ignoreCase ? key.toLowerCase() : key;
         ConfigEntry<T> configEntry = emptyConfigEntrySupplier.get();
         configEntry.setValue(value);
         entryMap.put(key, configEntry);
--- src/main/java/com/froobworld/nabconfiguration/ConfigSectionMap.java
+++ src/main/java/com/froobworld/nabconfiguration/ConfigSectionMap.java
@@ -8,11 +8,13 @@ public class ConfigSectionMap<K, C extends ConfigSection> {
     private final Function<K, String> keyMappingFunction;
     private final Map<String, C> sectionMap = new HashMap<>();
     private final Class<C> entryType;
+    private final boolean ignoreCase;
     private C defaultSection;
 
-    public ConfigSectionMap(Function<K, String> keyMappingFunction, Class<C> entryType) {
+    public ConfigSectionMap(Function<K, String> keyMappingFunction, Class<C> entryType, boolean ignoreCase) {
         this.keyMappingFunction = keyMappingFunction;
         this.entryType = entryType;
+        this.ignoreCase = ignoreCase;
     }
 
 
@@ -21,11 +23,14 @@ public class ConfigSectionMap<K, C extends ConfigSection> {
     }
 
     void put(String key, C configSection) {
+        key = ignoreCase ? key.toLowerCase() : key;
         sectionMap.put(key, configSection);
     }
 
     public C of(K key) {
-        return sectionMap.getOrDefault(keyMappingFunction.apply(key), defaultSection);
+        String mappedKey = keyMappingFunction.apply(key);
+        mappedKey = ignoreCase ? mappedKey.toLowerCase() : mappedKey;
+        return sectionMap.getOrDefault(mappedKey, defaultSection);
     }
 
     Class<C> entryType() {
